9ec63804ea02eee1b691fb88f88f40e90b9e6f42
Eli T. Drumm <etd@dte.li>
2019-10-01 10:07:29 -0400

    first commit

---
+++ 1-jsfundamentals/1-grammarAndTypes/02-types.js
@@ -0,0 +1,14 @@
+
+
+let myself = {
+  firstName: 'Eli',
+  lastName: 'Drumm',
+  middleInit: 'T',
+  houseNumber: 6295,
+  street: 'Dover Ct.',
+  city: 'Fishers',
+  state: 'IN',
+  zipcode: '46038'
+}
+
+console.log(myself.firstName, myself.middleInit, myself.lastName, '\n', myself.houseNumber, myself.street, '\n', myself.city, ' ', myself.state, myself.zipcode)
---
+++ scratch/test01.js
@@ -0,0 +1,4 @@
+
+let y = 3;
+let x = `it's ${y}`;
+console.log(x);
